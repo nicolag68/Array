708bacbb0ddf469ccc6a195e694bcefb3c82ea57
Nicola <14530018@itisrossi.vi.it>
2025-12-03 12:35:53 +0100

    ...

--- Array/Code/main.js
+++ Array/Code/main.js
@@ -10,8 +10,8 @@ let v3 = [];
 //Aggiunge VALORI numeri random ai vettori
 function addRandom() {
     for (let i = 0; i < VALORI; i++) {
-        v1.push(Math.floor(Math.random() * NUM_MAX));
-        v2.push(Math.floor(Math.random() * NUM_MAX));
+        v1.push(Math.floor(Math.random() * NUM_MAX + 1));
+        v2.push(Math.floor(Math.random() * NUM_MAX + 1));
     }
 }
 
@@ -19,26 +19,30 @@ function addRandom() {
 function stampaArray() {
     document.getElementById("v1").innerText = v1;
     document.getElementById("v2").innerText = v2;
+    document.getElementById("v3").innerText = v3;
 }
 
 //Funzione BubbleSort
 function ordinaBubble(array) {
-    let nigegr = array.length -1;
-    while(nigegr > 0) {
-        for (let i = 0; i < nigegr; i++) {
+    let lenght = array.length -1;
+    while(lenght > 0) {
+        for (let i = 0; i < lenght; i++) {
             if (array[i] > array[i + 1]) {
                 let temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
             }
         }
-        nigegr--;
+        lenght--;
     }
 }
 
 //Funzione fusione array
 function fusionArray() {
+    lunghezza = v1.length + v2.length - 2;
+    while(lunghezza > 0) {
 
+    }
 }
 
 //Funzione che inizializza il codice
